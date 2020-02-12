6bdfd0bdd45bbe879b3e48e7f410b7482a9179bd
Weihua Hu <weihua916@gmail.com>
2020-02-11 23:15:49 -0800

    revert url.py

--- ogb/utils/url.py
+++ ogb/utils/url.py
@@ -1,13 +1,15 @@
+import urllib.request as ur
 import zipfile
 import os
 import os.path as osp
+from six.moves import urllib
 import errno
-import requests
 
 def decide_download(url):
-    size_byte = int(requests.get(url).headers["content-length"])
+    d = ur.urlopen(url)
     GBFACTOR = float(1 << 30)
-    size = size_byte/GBFACTOR
+    size = int(d.info()["Content-Length"])/GBFACTOR
+
     ### confirm if larger than 1GB
     if size > 1:
         return input("This will download %.2fGB. Will you proceed? (y/N)\n" % (size)).lower() == "y"
@@ -32,9 +34,8 @@ def download_url(url, folder, log=True):
 
     filename = url.rpartition('/')[2]
     path = osp.join(folder, filename)
-    tmp_download_path = osp.join(folder, "_" + filename)
 
-    if osp.exists(path):  # pragma: no cover
+    if osp.exists(path) and osp.getsize(path) > 0:  # pragma: no cover
         if log:
             print('Using exist file', filename)
         return path
@@ -43,24 +44,11 @@ def download_url(url, folder, log=True):
         print('Downloading', url)
 
     makedirs(folder)
+    data = urllib.request.urlopen(url)
 
-    if osp.exists(tmp_download_path):
-        resume_header = {'Range': 'bytes=%d-' % osp.getsize(tmp_download_path)}
-        mode = "ab"
-    else:
-        resume_header = {}
-        mode = "wb"
+    with open(path, 'wb') as f:
+        f.write(data.read())
 
-    with requests.get(url,
-                      headers=resume_header,
-                      stream=True,
-                      verify=False,
-                      allow_redirects=True) as r:
-        with open(tmp_download_path, mode) as f:
-            for chunk in r.iter_content(chunk_size=512 * 1024):
-                if chunk:
-                    f.write(chunk)
-    os.rename(tmp_download_path, path)
     return path
 
 def maybe_log(path, log=True):
@@ -83,4 +71,3 @@ if __name__ == "__main__":
     url = "https://ogb.stanford.edu/data/pyg_mol_download/tox21.zip"
     ans = decide_download(url)
     print(ans)
-
