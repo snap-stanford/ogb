68d07cd2250e9347ae9143d1033aec51aae8f022
Weihua Hu <weihua916@gmail.com>
2020-02-11 23:56:16 -0800

    platform-agnostic data loaders

---
+++ ogb/graphproppred/dataset.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import shutil, os
+import os.path as osp
+from ogb.utils.url import decide_download, download_url, extract_zip
+from ogb.io.read_graph_raw import read_csv_graph_raw
+import pickle
+
+class GraphPropPredDataset(object):
+    def __init__(self, name, root = "dataset"):
+        self.name = name ## original name, e.g., ogbg-mol-tox21
+        self.dir_name = "_".join(name.split("-")) ## replace hyphen with underline, e.g., ogbg_mol_tox21
+
+        self.original_root = root
+        self.root = osp.join(root, self.dir_name)
+
+        self.meta_info = pd.read_csv(os.path.join(os.path.dirname(__file__), "master.csv"), index_col = 0)
+        if not self.name in self.meta_info:
+            print(self.name)
+            error_mssg = "Invalid dataset name {}.\n".format(self.name)
+            error_mssg += "Available datasets are as follows:\n"
+            error_mssg += "\n".join(self.meta_info.keys())
+            raise ValueError(error_mssg)
+
+        self.download_name = self.meta_info[self.name]["download_name"] ## name of downloaded file, e.g., tox21
+
+        self.num_tasks = int(self.meta_info[self.name]["num tasks"])
+        self.task_type = self.meta_info[self.name]["task type"]
+
+        super(GraphPropPredDataset, self).__init__()
+
+        self.pre_process()
+
+    def pre_process(self):
+        processed_dir = osp.join(self.root, 'processed')
+        raw_dir = osp.join(self.root, 'raw')
+        pre_processed_file_path = osp.join(processed_dir, 'data_processed')
+
+        if os.path.exists(pre_processed_file_path):
+            loaded_dict = pickle.load(open(pre_processed_file_path, 'rb'))
+            self.graphs, self.labels = loaded_dict['graphs'], loaded_dict['labels']
+
+        else:
+            ### download
+            url = self.meta_info[self.name]["url"]
+            if decide_download(url):
+                path = download_url(url, self.original_root)
+                extract_zip(path, self.original_root)
+                os.unlink(path)
+                # delete folder if there exists
+                try:
+                    shutil.rmtree(self.root)
+                except:
+                    pass
+                shutil.move(osp.join(self.original_root, self.download_name), self.root)
+            else:
+                print("Stop download.")
+                exit(-1)
+
+            ### preprocess
+            add_inverse_edge = self.meta_info[self.name]["add_inverse_edge"] == "True"
+            graphs = read_csv_graph_raw(raw_dir, add_inverse_edge = add_inverse_edge)
+            labels = pd.read_csv(osp.join(raw_dir, "graph-label.csv.gz"), compression="gzip", header = None).values
+
+            pickle.dump({'graphs': graphs, 'labels': labels}, open(pre_processed_file_path, 'wb'), protocol=4)
+
+            ### load preprocessed files
+            loaded_dict = pickle.load(open(pre_processed_file_path, 'rb'))
+            self.graphs, self.labels = loaded_dict['graphs'], loaded_dict['labels']
+
+
+    def get_idx_split(self):
+        split_type = self.meta_info[self.name]["split"]
+        path = osp.join(self.root, "split", split_type)
+
+        train_idx = pd.read_csv(osp.join(path, "train.csv.gz"), compression="gzip", header = None).values.T[0]
+        valid_idx = pd.read_csv(osp.join(path, "valid.csv.gz"), compression="gzip", header = None).values.T[0]
+        test_idx = pd.read_csv(osp.join(path, "test.csv.gz"), compression="gzip", header = None).values.T[0]
+
+        return {"train": train_idx, "valid": valid_idx, "test": test_idx}
+
+    def __getitem__(self, idx):
+        """Get datapoint with index"""
+
+        if isinstance(idx, int):
+            return self.graphs[idx], self.labels[idx]
+
+        raise IndexError(
+            'Only integer is valid index (got {}).'.format(type(idx).__name__))
+
+    def __len__(self):
+        """Length of the dataset
+        Returns
+        -------
+        int
+            Length of Dataset
+        """
+        return len(self.graphs)
+
+    def __repr__(self):  # pragma: no cover
+        return '{}({})'.format(self.__class__.__name__, len(self))
+
+
+if __name__ == "__main__":
+    dataset = GraphPropPredDataset(name = "ogbg-mol-tox21")
+    splitted_index = dataset.get_idx_split()
+    print(dataset)
+    print(dataset[0])
+    print(splitted_index["train"])
+    print(splitted_index["valid"])
+    print(splitted_index["test"])
+
+
---
+++ ogb/linkproppred/dataset.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import shutil, os
+import os.path as osp
+from ogb.utils.url import decide_download, download_url, extract_zip
+from ogb.io.read_graph_raw import read_csv_graph_raw
+import pickle
+
+class LinkPropPredDataset(object):
+    def __init__(self, name, root = "dataset"):
+        self.name = name ## original name, e.g., ogbl-ppa
+        self.dir_name = "_".join(name.split("-")) ## replace hyphen with underline, e.g., ogbl_ppa
+
+        self.original_root = root
+        self.root = osp.join(root, self.dir_name)
+
+        self.meta_info = pd.read_csv(os.path.join(os.path.dirname(__file__), "master.csv"), index_col = 0)
+        if not self.name in self.meta_info:
+            print(self.name)
+            error_mssg = "Invalid dataset name {}.\n".format(self.name)
+            error_mssg += "Available datasets are as follows:\n"
+            error_mssg += "\n".join(self.meta_info.keys())
+            raise ValueError(error_mssg)
+
+        self.download_name = self.meta_info[self.name]["download_name"] ## name of downloaded file, e.g., ppassoc
+
+        self.task_type = self.meta_info[self.name]["task type"]
+
+        super(LinkPropPredDataset, self).__init__()
+
+        self.pre_process()
+
+    def pre_process(self):
+        processed_dir = osp.join(self.root, 'processed')
+        pre_processed_file_path = osp.join(processed_dir, 'data_processed')
+
+        if osp.exists(pre_processed_file_path):
+            self.graph = pickle.load(open(pre_processed_file_path, 'rb'))
+
+        else:
+            ### check download
+            if not osp.exists(osp.join(self.root, "raw", "edge.csv.gz")):
+                url = self.meta_info[self.name]["url"]
+                if decide_download(url):
+                    path = download_url(url, self.original_root)
+                    extract_zip(path, self.original_root)
+                    os.unlink(path)
+                    # delete folder if there exists
+                    try:
+                        shutil.rmtree(self.root)
+                    except:
+                        pass
+                    shutil.move(osp.join(self.original_root, self.download_name), self.root)
+                else:
+                    print("Stop download.")
+                    exit(-1)
+
+            raw_dir = osp.join(self.root, "raw")
+
+            ### pre-process and save
+            add_inverse_edge = self.meta_info[self.name]["add_inverse_edge"] == "True"
+            graph = read_csv_graph_raw(raw_dir, add_inverse_edge = add_inverse_edge)[0] # only a single graph
+
+            pickle.dump(graph, open(pre_processed_file_path, 'wb'), protocol=4)
+
+            self.graph = pickle.load(open(pre_processed_file_path, 'rb'))
+
+    def get_edge_split(self):
+        split_type = self.meta_info[self.name]["split"]
+        path = osp.join(self.root, "split", split_type)
+
+        train_idx = pd.read_csv(osp.join(path, "train.csv.gz"), compression="gzip", header = None).values
+        valid_idx = pd.read_csv(osp.join(path, "valid.csv.gz"), compression="gzip", header = None).values
+        test_idx = pd.read_csv(osp.join(path, "test.csv.gz"), compression="gzip", header = None).values
+
+        return {"train_edge": train_idx[:,:2], "train_edge_label": train_idx[:,2],
+                    "valid_edge": valid_idx[:,:2], "valid_edge_label": valid_idx[:,2], 
+                        "test_edge": test_idx[:,:2], "test_edge_label": test_idx[:,2]}
+
+    def __getitem__(self, idx):
+        assert idx == 0, "This dataset has only one graph"
+        return self.graph
+
+    def __len__(self):
+        return 1
+
+    def __repr__(self):  # pragma: no cover
+        return '{}({})'.format(self.__class__.__name__, len(self))
+
+if __name__ == "__main__":
+    dataset = LinkPropPredDataset(name = "ogbl-reviews")
+    splitted_edge = dataset.get_edge_split()
+    print(dataset[0])
+    print(splitted_edge)
