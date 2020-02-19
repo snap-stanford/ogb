be3157053ab015d378a9f8574e4ffa76aaef0463
Weihua Hu <weihua916@gmail.com>
2020-02-19 15:28:46 -0800

    update ogbl-ppa. include species id as input node feat

--- ogb/linkproppred/make_master_file.py
+++ ogb/linkproppred/make_master_file.py
@@ -8,11 +8,11 @@ dataset_list = []
 ### add meta-information about protein function prediction task
 name = "ogbl-ppa"
 dataset_dict[name] = {"task type": "link prediction"}
-dataset_dict[name]["download_name"] = "ppassoc_v2"
+dataset_dict[name]["download_name"] = "ppassoc_v3"
 dataset_dict[name]["url"] = "https://ogb.stanford.edu/data/linkproppred/"+dataset_dict[name]["download_name"]+".zip"
 ## For undirected grarph, we only store one directional information. This flag allows us to add inverse edge at pre-processing time
 dataset_dict[name]["add_inverse_edge"] = True 
-dataset_dict[name]["has_node_attr"] = False
+dataset_dict[name]["has_node_attr"] = True
 dataset_dict[name]["has_edge_attr"] = False
 dataset_dict[name]["split"] = "throughput"
 dataset_dict[name]["num nodes"] = 576289
