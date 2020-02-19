698f95680e47d45cbabe5d0c70e1fa4e40038807
Weihua Hu <weihua916@gmail.com>
2020-02-19 15:19:58 -0800

    update ogb-proteins. include species id as input node feat

--- ogb/nodeproppred/make_master_file.py
+++ ogb/nodeproppred/make_master_file.py
@@ -8,11 +8,11 @@ dataset_list = []
 ### add meta-information about protein function prediction task
 name = "ogbn-proteins"
 dataset_dict[name] = {"num tasks": 112, "task type": "binary classification"}
-dataset_dict[name]["download_name"] = "proteinfunc_v2"
+dataset_dict[name]["download_name"] = "proteinfunc_v3"
 dataset_dict[name]["url"] = "https://ogb.stanford.edu/data/nodeproppred/"+dataset_dict[name]["download_name"]+".zip"
 ## For undirected grarph, we only store one directional information. This flag allows us to add inverse edge at pre-processing time
 dataset_dict[name]["add_inverse_edge"] = True 
-dataset_dict[name]["has_node_attr"] = False
+dataset_dict[name]["has_node_attr"] = True
 dataset_dict[name]["has_edge_attr"] = True
 dataset_dict[name]["split"] = "species"
 dataset_dict[name]["num nodes"] = 132534
