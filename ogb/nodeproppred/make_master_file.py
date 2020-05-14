c891083d155e9402497bb7a77d7de952e8394838
Weihua Hu <weihua916@gmail.com>
2020-05-14 03:48:32 -0700

    add ogbn-mag preliminary

--- ogb/nodeproppred/make_master_file.py
+++ ogb/nodeproppred/make_master_file.py
@@ -16,11 +16,11 @@ dataset_dict[name]["add_inverse_edge"] = True
 dataset_dict[name]["has_node_attr"] = False
 dataset_dict[name]["has_edge_attr"] = True
 dataset_dict[name]["split"] = "species"
-dataset_dict[name]["num nodes"] = 132534
 dataset_dict[name]["additional node files"] = 'species'
 dataset_dict[name]['additional edge files'] = 'None'
+dataset_dict[name]['is hetero'] = False
 
-### add meta-information about protein function prediction task
+### add meta-information about product category prediction task
 name = "ogbn-products"
 dataset_dict[name] = {"num tasks": 1, "num classes": 47, "eval metric": "accuracy", "task type": "multiclass classification"}
 dataset_dict[name]["download_name"] = "products"
@@ -31,24 +31,37 @@ dataset_dict[name]["add_inverse_edge"] = True
 dataset_dict[name]["has_node_attr"] = True
 dataset_dict[name]["has_edge_attr"] = False
 dataset_dict[name]["split"] = "sales_ranking"
-dataset_dict[name]["num nodes"] = 2449029
 dataset_dict[name]["additional node files"] = 'None'
 dataset_dict[name]['additional edge files'] = 'None'
+dataset_dict[name]['is hetero'] = False
 
-### add meta-information about protein function prediction task
+### add meta-information about arxiv category prediction task
 name = "ogbn-arxiv"
 dataset_dict[name] = {"num tasks": 1, "num classes": 40, "eval metric": "accuracy", "task type": "multiclass classification"}
 dataset_dict[name]["download_name"] = "arxiv"
 dataset_dict[name]["version"] = 1
 dataset_dict[name]["url"] = "https://snap.stanford.edu/ogb/data/nodeproppred/"+dataset_dict[name]["download_name"]+".zip"
-## For undirected grarph, we only store one directional information. This flag allows us to add inverse edge at pre-processing time
 dataset_dict[name]["add_inverse_edge"] = False 
 dataset_dict[name]["has_node_attr"] = True
 dataset_dict[name]["has_edge_attr"] = False
 dataset_dict[name]["split"] = "time"
-dataset_dict[name]["num nodes"] = 169343
 dataset_dict[name]["additional node files"] = 'node_year'
 dataset_dict[name]['additional edge files'] = 'None'
+dataset_dict[name]['is hetero'] = False
+
+### add meta-information about paper venue prediction task
+name = "ogbn-mag"
+dataset_dict[name] = {"num tasks": 1, "num classes": 349, "eval metric": "accuracy", "task type": "multiclass classification"}
+dataset_dict[name]["download_name"] = "mag"
+dataset_dict[name]["version"] = 0
+dataset_dict[name]["url"] = "https://snap.stanford.edu/ogb/data/nodeproppred/"+dataset_dict[name]["download_name"]+".zip"
+dataset_dict[name]["add_inverse_edge"] = False 
+dataset_dict[name]["has_node_attr"] = True
+dataset_dict[name]["has_edge_attr"] = False
+dataset_dict[name]["split"] = "time"
+dataset_dict[name]["additional node files"] = 'node_year'
+dataset_dict[name]['additional edge files'] = 'edge_reltype'
+dataset_dict[name]['is hetero'] = True
 
 df = pd.DataFrame(dataset_dict)
 # saving the dataframe 
