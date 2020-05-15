56c73c94c566eee7d039a52846a5f74b2ea4a32a
Weihua Hu <weihua916@gmail.com>
2020-05-14 22:05:26 -0700

    add ogbl-biokg preliminary

--- ogb/linkproppred/make_master_file.py
+++ ogb/linkproppred/make_master_file.py
@@ -16,9 +16,9 @@ dataset_dict[name]["add_inverse_edge"] = True
 dataset_dict[name]["has_node_attr"] = True
 dataset_dict[name]["has_edge_attr"] = False
 dataset_dict[name]["split"] = "throughput"
-dataset_dict[name]["num nodes"] = 576289
 dataset_dict[name]["additional node files"] = 'None'
 dataset_dict[name]['additional edge files'] = 'None'
+dataset_dict[name]['is hetero'] = False
 
 ### add meta-information about author collaboration prediction task
 name = "ogbl-collab"
@@ -31,9 +31,9 @@ dataset_dict[name]["add_inverse_edge"] = True
 dataset_dict[name]["has_node_attr"] = True
 dataset_dict[name]["has_edge_attr"] = False
 dataset_dict[name]["split"] = "time"
-dataset_dict[name]["num nodes"] = 235868
 dataset_dict[name]["additional node files"] = 'None'
 dataset_dict[name]['additional edge files'] = 'edge_weight,edge_year'
+dataset_dict[name]['is hetero'] = False
 
 ### add meta-information about paper citation recommendation task
 name = "ogbl-citation"
@@ -46,13 +46,13 @@ dataset_dict[name]["add_inverse_edge"] = False
 dataset_dict[name]["has_node_attr"] = True
 dataset_dict[name]["has_edge_attr"] = False
 dataset_dict[name]["split"] = "time"
-dataset_dict[name]["num nodes"] = 2927963
 dataset_dict[name]["additional node files"] = 'node_year'
 dataset_dict[name]['additional edge files'] = 'None'
+dataset_dict[name]['is hetero'] = False
 
 ### add meta-information about wikidata knowledge graph completion task
 name = "ogbl-wikikg"
-dataset_dict[name] = {"eval metric": "mrr", "task type": "link prediction"}
+dataset_dict[name] = {"eval metric": "mrr", "task type": "KG completion"}
 dataset_dict[name]["download_name"] = "wikikg"
 dataset_dict[name]["version"] = 1
 dataset_dict[name]["url"] = "https://snap.stanford.edu/ogb/data/linkproppred/"+dataset_dict[name]["download_name"]+".zip"
@@ -61,11 +61,11 @@ dataset_dict[name]["add_inverse_edge"] = False
 dataset_dict[name]["has_node_attr"] = False
 dataset_dict[name]["has_edge_attr"] = False
 dataset_dict[name]["split"] = "time"
-dataset_dict[name]["num nodes"] = 2500604 # number of relations is 535
 dataset_dict[name]["additional node files"] = 'None'
 dataset_dict[name]['additional edge files'] = 'edge_reltype'
+dataset_dict[name]['is hetero'] = False
 
-### add meta-information about wikidata knowledge graph completion task
+### add meta-information about drug drug interaction prediction task
 name = "ogbl-ddi"
 dataset_dict[name] = {"eval metric": "hits@10", "task type": "link prediction"}
 dataset_dict[name]["download_name"] = "ddi"
@@ -76,9 +76,24 @@ dataset_dict[name]["add_inverse_edge"] = True
 dataset_dict[name]["has_node_attr"] = False
 dataset_dict[name]["has_edge_attr"] = False
 dataset_dict[name]["split"] = "target"
-dataset_dict[name]["num nodes"] = 4267
 dataset_dict[name]["additional node files"] = 'None'
 dataset_dict[name]['additional edge files'] = 'None'
+dataset_dict[name]['is hetero'] = False
+
+### add meta-information about biological knowledge graph completion task
+name = "ogbl-biokg"
+dataset_dict[name] = {"eval metric": "mrr", "task type": "KG completion"}
+dataset_dict[name]["download_name"] = "biokg"
+dataset_dict[name]["version"] = 0
+dataset_dict[name]["url"] = "https://snap.stanford.edu/ogb/data/linkproppred/"+dataset_dict[name]["download_name"]+".zip"
+## For undirected grarph, we only store one directional information. This flag allows us to add inverse edge at pre-processing time
+dataset_dict[name]["add_inverse_edge"] = False 
+dataset_dict[name]["has_node_attr"] = False
+dataset_dict[name]["has_edge_attr"] = False
+dataset_dict[name]["split"] = "random"
+dataset_dict[name]["additional node files"] = 'None'
+dataset_dict[name]['additional edge files'] = 'edge_reltype'
+dataset_dict[name]['is hetero'] = True
 
 df = pd.DataFrame(dataset_dict)
 # saving the dataframe 
