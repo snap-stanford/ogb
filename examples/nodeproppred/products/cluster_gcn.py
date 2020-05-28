cf066f93311ab3099cad84d71085d1b0375dcc2e
rusty1s <matthias.fey@tu-dortmund.de>
2020-05-28 09:30:24 +0200

    updated cluster-gcn hyperparameters

--- examples/nodeproppred/products/cluster_gcn.py
+++ examples/nodeproppred/products/cluster_gcn.py
@@ -1,11 +1,10 @@
 import argparse
 
 import torch
-import numpy as np
+from tqdm import tqdm
 import torch.nn.functional as F
 
-from torch_sparse import SparseTensor
-from torch_geometric.data import ClusterData, ClusterLoader
+from torch_geometric.data import ClusterData, ClusterLoader, NeighborSampler
 from torch_geometric.nn import SAGEConv
 
 from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
@@ -38,30 +37,43 @@ class SAGE(torch.nn.Module):
         x = self.convs[-1](x, edge_index)
         return torch.log_softmax(x, dim=-1)
 
+    def inference(self, x_all, subgraph_loader, device):
+        pbar = tqdm(total=x_all.size(0) * len(self.convs))
+        pbar.set_description('Evaluating')
 
-class SAGEInference(torch.nn.Module):
-    def __init__(self, weights):
-        super(SAGEInference, self).__init__()
-        self.weights = weights
+        for i, conv in enumerate(self.convs):
+            xs = []
+            for batch_size, n_id, adj in subgraph_loader:
+                edge_index, _, size = adj.to(device)
+                x = x_all[n_id].to(device)
+                x_target = x[:size[1]]
+                x = conv((x, x_target), edge_index)
+                if i != len(self.convs) - 1:
+                    x = F.relu(x)
+                xs.append(x.cpu())
 
-    def forward(self, x, adj):
-        out = x
-        for i, (weight, bias, root_weight) in enumerate(self.weights):
-            tmp = adj @ out @ weight
-            out = tmp + out @ root_weight + bias
-            out = np.clip(out, 0, None) if i < len(self.weights) - 1 else out
-        return out
+                pbar.update(batch_size)
+
+            x_all = torch.cat(xs, dim=0)
+
+        pbar.close()
+
+        return x_all
 
 
 def train(model, loader, optimizer, device):
     model.train()
 
     total_loss = total_examples = 0
+    total_correct = total_examples = 0
     for data in loader:
         data = data.to(device)
+        if data.train_mask.sum() == 0:
+            continue
         optimizer.zero_grad()
         out = model(data.x, data.edge_index)[data.train_mask]
-        loss = F.nll_loss(out, data.y.squeeze(1)[data.train_mask])
+        y = data.y.squeeze(1)[data.train_mask]
+        loss = F.nll_loss(out, y)
         loss.backward()
         optimizer.step()
 
@@ -69,28 +81,20 @@ def train(model, loader, optimizer, device):
         total_loss += loss.item() * num_examples
         total_examples += num_examples
 
-    return total_loss / total_examples
+        total_correct += out.argmax(dim=-1).eq(y).sum().item()
+        total_examples += y.size(0)
+
+    return total_loss / total_examples, total_correct / total_examples
 
 
 @torch.no_grad()
-def test(model, data, evaluator):
-    print('Evaluating full-batch GNN on CPU...')
+def test(model, data, evaluator, subgraph_loader, device):
+    model.eval()
 
-    weights = [(conv.lin_rel.weight.t().cpu().detach().numpy(),
-                conv.lin_rel.bias.cpu().detach().numpy(),
-                conv.lin_root.weight.t().cpu().detach().numpy())
-               for conv in model.convs]
-    model = SAGEInference(weights)
-
-    x = data.x.numpy()
-    adj = SparseTensor(row=data.edge_index[0], col=data.edge_index[1])
-    adj = adj.sum(dim=1).pow(-1).view(-1, 1) * adj
-    adj = adj.to_scipy(layout='csr')
-
-    out = model(x, adj)
+    out = model.inference(data.x, subgraph_loader, device)
 
     y_true = data.y
-    y_pred = torch.from_numpy(out).argmax(dim=-1, keepdim=True)
+    y_pred = out.argmax(dim=-1, keepdim=True)
 
     train_acc = evaluator.eval({
         'y_true': y_true[data.train_mask],
@@ -112,15 +116,15 @@ def main():
     parser = argparse.ArgumentParser(description='OGBN-Products (Cluster-GCN)')
     parser.add_argument('--device', type=int, default=0)
     parser.add_argument('--log_steps', type=int, default=1)
-    parser.add_argument('--num_partitions', type=int, default=5000)
-    parser.add_argument('--num_workers', type=int, default=6)
+    parser.add_argument('--num_partitions', type=int, default=15000)
+    parser.add_argument('--num_workers', type=int, default=12)
     parser.add_argument('--num_layers', type=int, default=3)
     parser.add_argument('--hidden_channels', type=int, default=256)
     parser.add_argument('--dropout', type=float, default=0.5)
-    parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--epochs', type=int, default=50)
-    parser.add_argument('--eval_steps', type=int, default=10)
+    parser.add_argument('--eval_steps', type=int, default=5)
     parser.add_argument('--runs', type=int, default=10)
     args = parser.parse_args()
     print(args)
@@ -144,6 +148,10 @@ def main():
     loader = ClusterLoader(cluster_data, batch_size=args.batch_size,
                            shuffle=True, num_workers=args.num_workers)
 
+    subgraph_loader = NeighborSampler(data.edge_index, sizes=[-1],
+                                      batch_size=1024, shuffle=False,
+                                      num_workers=args.num_workers)
+
     model = SAGE(data.x.size(-1), args.hidden_channels, dataset.num_classes,
                  args.num_layers, args.dropout).to(device)
 
@@ -154,14 +162,15 @@ def main():
         model.reset_parameters()
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
         for epoch in range(1, 1 + args.epochs):
-            loss = train(model, loader, optimizer, device)
+            loss, train_acc = train(model, loader, optimizer, device)
             if epoch % args.log_steps == 0:
                 print(f'Run: {run + 1:02d}, '
                       f'Epoch: {epoch:02d}, '
-                      f'Loss: {loss:.4f}')
+                      f'Loss: {loss:.4f}, '
+                      f'Approx Train Acc: {train_acc:.4f}')
 
-            if epoch % args.eval_steps == 0:
-                result = test(model, data, evaluator)
+            if epoch > 19 and epoch % args.eval_steps == 0:
+                result = test(model, data, evaluator, subgraph_loader, device)
                 logger.add_result(run, result)
                 train_acc, valid_acc, test_acc = result
                 print(f'Run: {run + 1:02d}, '
