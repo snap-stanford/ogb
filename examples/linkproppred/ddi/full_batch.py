0982899a568d5d521a66b35579e5b252dda3a34c
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-25 09:22:11 +0200

    less epochs

--- examples/linkproppred/ddi/full_batch.py
+++ examples/linkproppred/ddi/full_batch.py
@@ -252,7 +252,7 @@ def main():
     parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--batch_size', type=int, default=64 * 1024)
     parser.add_argument('--lr', type=float, default=0.005)
-    parser.add_argument('--epochs', type=int, default=300)
+    parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--eval_steps', type=int, default=5)
     parser.add_argument('--runs', type=int, default=10)
     args = parser.parse_args()
