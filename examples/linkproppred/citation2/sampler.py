7085b3020e8eb64f494f810fcd1a8fdaed2ab18d
Weihua Hu <weihua916@gmail.com>
2020-12-25 22:59:46 -0800

    neigh sampler to citation2

--- examples/linkproppred/citation2/sampler.py
+++ examples/linkproppred/citation2/sampler.py
@@ -204,7 +204,7 @@ def test(model, predictor, x, subgraph_loader, split_edge, evaluator,
 
 
 def main():
-    parser = argparse.ArgumentParser(description='OGBL-Citation (NS)')
+    parser = argparse.ArgumentParser(description='OGBL-Citation2 (NS)')
     parser.add_argument('--device', type=int, default=0)
     parser.add_argument('--log_steps', type=int, default=1)
     parser.add_argument('--num_workers', type=int, default=12)
@@ -222,7 +222,7 @@ def main():
     device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
     device = torch.device(device)
 
-    dataset = PygLinkPropPredDataset(name='ogbl-citation')
+    dataset = PygLinkPropPredDataset(name='ogbl-citation2')
     split_edge = dataset.get_edge_split()
     data = dataset[0]
     edge_index = to_undirected(data.edge_index, data.num_nodes)
@@ -256,7 +256,7 @@ def main():
     predictor = LinkPredictor(args.hidden_channels, args.hidden_channels, 1,
                               args.num_layers, args.dropout).to(device)
 
-    evaluator = Evaluator(name='ogbl-citation')
+    evaluator = Evaluator(name='ogbl-citation2')
     logger = Logger(args.runs, args)
 
     for run in range(args.runs):
