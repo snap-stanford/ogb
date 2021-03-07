6d94a6894174b7ad2c487da558b27aca405453d3
Weihua Hu <weihua916@gmail.com>
2021-03-07 15:45:25 -0800

    Make pcqm4m dataset libary-agnostic

--- ogb/lsc/__init__.py
+++ ogb/lsc/__init__.py
@@ -1,3 +1,14 @@
-from .pcqm4m import PyGPCQM4MDataset, PCQM4MEvaluator
+from .pcqm4m import PCQM4MDataset, PCQM4MEvaluator
+
+try:
+    from .pcqm4m_pyg import PygPCQM4MDataset
+except ImportError:
+    pass
+
+try:
+    from .pcqm4m_dgl import DglPCQM4MDataset
+except (ImportError, OSError):
+    pass
+
 from .mag240m import MAG240MDataset, MAG240MEvaluator
 from .wikikg90m import WikiKG90MDataset, WikiKG90MEvaluator
