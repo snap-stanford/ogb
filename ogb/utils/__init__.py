7a15490a1b16787ae46d29f225c1ce5aebef486b
Victor Apolonio <victor@apolonio.dev>
2021-05-01 16:10:58 -0300

    Includes better error handling when import smiles2graph

--- ogb/utils/__init__.py
+++ ogb/utils/__init__.py
@@ -1,4 +1,5 @@
 try:
-    from .mol import smiles2graph
-except ImportError:
-    pass
+    from ogb.mol import smiles2graph
+except Exception as e:
+    print("Error on imports, verify if all dependencies are installed correctly.")
+    raise e
