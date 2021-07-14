ee47b092aaa0839f01f2ec681a23cb2e658818a4
Weihua <weihua916@gmail.com>
2021-07-14 13:55:48 -0700

    bug fix

--- ogb/lsc/utils.py
+++ ogb/lsc/utils.py
@@ -3,6 +3,12 @@ import numpy as np
 
 # Assigning the whole test into test-dev and test-challenge
 def split_test(split_dict):
+    if 'test' not in split_dict:
+        assert 'test-whole' in split_dict
+        assert 'test-dev' in split_dict
+        assert 'test-challenge' in split_dict
+        return 
+
     if isinstance(split_dict['test'], torch.Tensor):
         idx = torch.arange(len(split_dict['test']))
         dev_idx = torch.nonzero(idx % 5 < 3, as_tuple=True)[0]
