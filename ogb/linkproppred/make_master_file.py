dd2a62a2e563603a2987d051e4f52b1d4b6dd6e4
Weihua Hu <weihua916@gmail.com>
2022-07-16 09:42:37 -0700

    add ogbl-vessel

--- ogb/linkproppred/make_master_file.py
+++ ogb/linkproppred/make_master_file.py
@@ -103,6 +103,23 @@ dataset_dict[name]['additional edge files'] = 'edge_reltype'
 dataset_dict[name]['is hetero'] = True
 dataset_dict[name]['binary'] = False
 
+### add meta-information about vessel graph completion task
+name = 'ogbl-vessel'
+dataset_dict[name] = {'eval metric': 'rocauc', 'task type': 'link prediction'}
+dataset_dict[name]['download_name'] = 'vessel'
+dataset_dict[name]['version'] = 1
+dataset_dict[name]['url'] = 'http://snap.stanford.edu/ogb/data/linkproppred/'+dataset_dict[name]['download_name']+'.zip'
+## For undirected grarph, we only store one directional information. This flag allows us to add inverse edge at pre-processing time
+dataset_dict[name]['add_inverse_edge'] = False 
+dataset_dict[name]['has_node_attr'] = True
+dataset_dict[name]['has_edge_attr'] = False
+dataset_dict[name]['split'] = 'spatial'
+dataset_dict[name]['additional node files'] = 'None'
+dataset_dict[name]['additional edge files'] = 'None'
+dataset_dict[name]['is hetero'] = False
+dataset_dict[name]['binary'] = True
+
+
 df = pd.DataFrame(dataset_dict)
 # saving the dataframe 
 df.to_csv('master.csv')
