f5534d99703ab549ae4f7279f2002c6cc79041dc
Weihua Hu <weihua916@gmail.com>
2022-08-14 08:37:09 -0700

    test different sizes for y_pred_pos and y_pred_neg

--- ogb/linkproppred/evaluate.py
+++ ogb/linkproppred/evaluate.py
@@ -299,7 +299,7 @@ if __name__ == '__main__':
     evaluator = Evaluator(name = 'ogbl-vessel')
     print(evaluator.expected_input_format)
     print(evaluator.expected_output_format)
-    y_pred_pos = torch.tensor(np.random.randn(1000,))
+    y_pred_pos = torch.tensor(np.random.randn(1500,))
     y_pred_neg = torch.tensor(np.random.randn(1000,))
     input_dict = {'y_pred_pos': y_pred_pos, 'y_pred_neg': y_pred_neg}
     result = evaluator.eval(input_dict)
