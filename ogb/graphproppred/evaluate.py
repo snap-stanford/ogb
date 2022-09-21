dd9e3d0ed642619528e90d312bc8b52476338c64
Weihua Hu <weihua916@gmail.com>
2022-09-21 08:02:01 -0700

    Fix bug in rmse evaluation
    This should not affect any existing results because all regression datasets are single (not multi-) task, i.e., `y_true.shape[1]` is 1.

--- ogb/graphproppred/evaluate.py
+++ ogb/graphproppred/evaluate.py
@@ -215,7 +215,7 @@ class Evaluator:
         for i in range(y_true.shape[1]):
             # ignore nan values
             is_labeled = y_true[:,i] == y_true[:,i]
-            rmse_list.append(np.sqrt(((y_true[is_labeled] - y_pred[is_labeled])**2).mean()))
+            rmse_list.append(np.sqrt(((y_true[is_labeled,i] - y_pred[is_labeled,i])**2).mean()))
 
         return {'rmse': sum(rmse_list)/len(rmse_list)}
 
