3ac3dc4f8b669d40a2c045ebc05c214ba4e8d4cd
puririshi98 <puririshi98@berkeley.edu>
2023-06-05 13:19:10 -0700

    minor fixes to get it working

--- examples/lsc/mag240m/gnn.py
+++ examples/lsc/mag240m/gnn.py
@@ -189,7 +189,8 @@ if __name__ == '__main__':
                     num_layers=len(args.sizes), dropout=args.dropout)
         print(f'#Params {sum([p.numel() for p in model.parameters()])}')
         checkpoint_callback = ModelCheckpoint(monitor='val_acc', mode = 'max', save_top_k=1)
-        if torch.cuda.is_available():
+        # if torch.cuda.is_available():
+        if False:
             trainer = Trainer(accelerator='gpu', devices=8, max_epochs=args.epochs,
                               callbacks=[checkpoint_callback],
                               default_root_dir=f'logs/{args.model}',
