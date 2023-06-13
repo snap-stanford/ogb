56712dab208f6d7e6d9b8d166f7482e6d217814d
puririshi98 <puririshi98@berkeley.edu>
2023-06-13 13:46:57 -0700

    debugging

--- examples/lsc/mag240m/gnn.py
+++ examples/lsc/mag240m/gnn.py
@@ -203,7 +203,7 @@ if __name__ == '__main__':
         print(f'#Params {sum([p.numel() for p in model.parameters()])}')
         checkpoint_callback = ModelCheckpoint(dirpath=os.getcwd(), monitor='val_acc', mode = 'max', save_top_k=1)
         if torch.cuda.is_available():
-            trainer = Trainer(accelerator='gpu', devices=1, # devices=torch.cuda.device_count(),  strategy="ddp",
+            trainer = Trainer(accelerator='gpu', devices=torch.cuda.device_count(),  strategy="ddp", #devices=1, 
                               max_epochs=args.epochs,
                               callbacks=[checkpoint_callback],
                               default_root_dir=f'logs/{args.model}',
