adec78868b860884174cac7cedd5d67bbb7c160d
puririshi98 <puririshi98@berkeley.edu>
2023-06-30 16:49:39 -0700

    only use 1/10th the nodes but will add a flag to control the amount of data to use next week

--- examples/lsc/mag240m/gnn.py
+++ examples/lsc/mag240m/gnn.py
@@ -346,6 +346,7 @@ if __name__ == "__main__":
     print("Loading Data...")
     dataset = MAG240MDataset(ROOT)
     data = dataset.to_pyg_hetero_data()
+    print("Making a subgraph of the data to save and reduce hardware requirements")
     data = data.subgraph({n_type:torch.randperm(data[n_type].num_nodes)[:int(data[n_type].num_nodes/100)] for n_type in data.node_types})
     if not args.evaluate:
         if args.n_devices > 1:
