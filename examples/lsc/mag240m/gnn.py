5e6638e28eb3a000caeb64bc6471894867872aa3
puririshi98 <puririshi98@berkeley.edu>
2023-06-30 16:53:28 -0700

    only use 1/10th the nodes but will add a flag to control the amount of data to use next week

--- examples/lsc/mag240m/gnn.py
+++ examples/lsc/mag240m/gnn.py
@@ -347,7 +347,7 @@ if __name__ == "__main__":
     dataset = MAG240MDataset(ROOT)
     data = dataset.to_pyg_hetero_data()
     print("Making a subgraph of the data to save and reduce hardware requirements")
-    data = data.subgraph({n_type:torch.randperm(data[n_type].num_nodes)[:int(data[n_type].num_nodes/100)] for n_type in data.node_types})
+    data = data.subgraph({n_type:torch.randperm(data[n_type].num_nodes)[:int(data[n_type].num_nodes/1000)] for n_type in data.node_types})
     if not args.evaluate:
         if args.n_devices > 1:
             print("Let's use", args.n_devices, "GPUs!")
